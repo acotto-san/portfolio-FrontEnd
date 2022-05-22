c4db9723dd5a7c2ef55b458e7ed1847ae39a8c67
acotto-san <acostaottonelli.santiago@gmail.com>
2022-05-22 12:47:24 -0300

    ups

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  piBaseUrl:'https://argenprog-portfolio-backend.herokuapp.com'
+  apiBaseUrl:'https://argenprog-portfolio-backend.herokuapp.com'
   // apiBaseUrl: 'http://localhost:8080'
 };
